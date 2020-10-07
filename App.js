49d07f706fe9aa2e074b883b7f430c95b1f9b882
Gabriel Augusto Fontes da Silva <gabriel_fsilva@outlook.com.br>
2020-10-07 10:08:37 -0300

    feat: adds goal input and goal flatlist

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  FlatList,
+} from "react-native";
+
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput"
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const addGoalHandler = (goalTitle) => {
+    // Esta maneira pode haver erro
+    // setCourseGoals([...courseGoals, enteredGoal])
+    // Esta maneira sempre é garantida de funcionar
+    setCourseGoals((currentGoals) => [
+      ...currentGoals,
+      {
+        id: Math.random().toString(),
+        value: goalTitle,
+      },
+    ]);
+  };
+
+  const removeGoalHandler = (goalId) => {
+    setCourseGoals(
+      currentGoals => currentGoals.filter(goal => goal.id !== goalId)
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <GoalInput onAddGoal={addGoalHandler} />
+      <FlatList
+        keyExtractor={(item, index) => item.id} // Caso queira usar um nome diferente de key
+        data={courseGoals}
+        renderItem={
+          ({ item }) => (
+            <GoalItem 
+              onDelete={removeGoalHandler.bind(this, item.id)}
+              title={item.value}
+            />)
+        }
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+    backgroundColor: "#88335844",
   },
 });
